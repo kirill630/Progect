53322b028f35d0113f3fa82b4ecf6b6a1012bde7
Daniil <dan.games@inbox.ru>
2021-11-17 16:45:03 +0300

    Create Progect2

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send('<h1>Dream food</h1>')
+})
+
+app.listen(port);
